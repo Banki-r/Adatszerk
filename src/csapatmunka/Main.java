09e9e9bf19cb45bbaa29cb808499446a0564f842
kkristof11 <kovacskristof1895@gmail.com>
2021-03-09 13:59:57 +0100

    lengyel forma kiértékelése

--- src/csapatmunka/Main.java
+++ src/csapatmunka/Main.java
@@ -7,7 +7,7 @@ public class Main {
     
     
     public static void main(String[] args) {
-        System.out.println(lengyelFormaSzamol("15*"));
+        System.out.println(lengyelFormaSzamol("2695-*+"));
     }
     
     public static double lengyelFormaSzamol(String lengyelForma){
@@ -16,22 +16,21 @@ public class Main {
                 while(!lengyelForma.equals("")){
                     char elem = lengyelForma.charAt(0);
                     lengyelForma=lengyelForma.substring(1);
-                    System.out.println(lengyelForma);
                     if(!(elem=='+' || elem=='-' || elem=='*' || elem=='/')){
                         veremSeged.add(elem+"");
-                        System.out.println(elem);
                     }
-                    if(elem=='+' || elem=='-' || elem=='*' || elem=='/'){
-                        int ertek1=Integer.parseInt(veremSeged.pop());
-                        int ertek2=Integer.parseInt(veremSeged.pop());
-                        double eredmeny=kiszamol(ertek2,elem,ertek1);//kellmég
+                    else//if(elem=='+' || elem=='-' || elem=='*' || elem=='/'){
+                    {
+                        double ertek1=Double.parseDouble(veremSeged.pop());
+                        double ertek2=Double.parseDouble(veremSeged.pop());
+                        double eredmeny=kiszamol(ertek2,elem,ertek1);
                         veremSeged.add(eredmeny+"");
                     }
                 }
         return Double.parseDouble(veremSeged.pop());
     }
 
-    private static double kiszamol(int ertek2, char elem, int ertek1) {
+    private static double kiszamol(double ertek2, char elem, double ertek1) {
         double eredmeny=0;
         if(elem=='+')
             eredmeny = ertek2+ertek1;
